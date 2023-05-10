9b33fcfbf9ab83771c4a3400a1cad8cce37ab927
AungKo2019 <highflier.ako1991@gmail.com>
2023-05-11 00:44:47 +0630

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+// import { Provider } from 'next-auth/client'
+import Navbar from '@/components/Navbar'
+import '../styles/globals.css'
+import '../components/Navbar.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+     {/* <Provider session={pageProps.session}> */}
+      <Navbar />
+      <Component {...pageProps} />
+     {/* </Provider> */}
+    </>
+  )
 }
+
+export default MyApp
